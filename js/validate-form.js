aceb27cc7489f4f0fe7b6a219f84868d5523f9d9
Yury Kravchenko <yury.kravchenko.1987@gmail.com>
2022-07-09 18:07:50 +0300

    Add validate type

--- js/validate-form.js
+++ js/validate-form.js
@@ -1,14 +1,36 @@
 const adForm = document.querySelector('.ad-form');
 const rooms = adForm.querySelector('#room_number');
 const capacity = adForm.querySelector('#capacity');
+const fieldPrice = adForm.querySelector('#price');
+const type = adForm.querySelector('#type');
+const timeIn = adForm.querySelector('#timein');
+const timeOut = adForm.querySelector('#timeout');
 
 const LengthTitle = {
   MIN_SYMBOLS: 30,
   MAX_SYMBOLS: 100
 };
 
-const MaxPriceAd = {
+const MaxPriceAmount = {
   MAX_PRICE: 100000
+};
+
+const MinPriceAmount = {
+  MIN_PRICE: {
+    palace: 10000,
+    flat: 1000,
+    house: 5000,
+    bungalow: 0,
+    hotel: 3000
+  }
+};
+
+const ChangeWord = {
+  palace: 'дворца',
+  flat: 'квартиры',
+  house: 'дома',
+  bungalow: 'бунгало',
+  hotel: 'отеля'
 };
 
 const MaxCapacityGuestInRooms = {
@@ -51,14 +73,14 @@ pristine.addValidator(
  * @returns максимальная сумма за ночь
  */
 function validateMaxPriceAdForm (value) {
-  return value <= MaxPriceAd.MAX_PRICE;
+  return value <= MaxPriceAmount.MAX_PRICE;
 }
 
 /**
  * @returns String error максимальная сумма за ночь
  */
 function errorMessageMaxPrice () {
-  return `Максимальная сумма ${MaxPriceAd.MAX_PRICE}`;
+  return `Максимальная сумма ${MaxPriceAmount.MAX_PRICE}`;
 }
 
 //валидация цены за ночь
@@ -81,6 +103,44 @@ pristine.addValidator(
   validateCapacityGuestinRooms,
   errorMessageCapacityGuestInRooms
 );
+
+type.addEventListener('change', () => {
+  fieldPrice.placeholder = MinPriceAmount.MIN_PRICE[type.value];
+  fieldPrice.min = MinPriceAmount.MIN_PRICE[type.value];
+  fieldPrice.value = '';
+});
+
+function validatePrice (value) {
+  return value <= MaxPriceAmount.MAX_PRICE[type.value] && value >= MinPriceAmount.MIN_PRICE[type.value];
+}
+
+function errorMessagePrice () {
+  return (fieldPrice.value > MaxPriceAmount.MAX_PRICE[type.value])
+    ? `Стоимость ${ChangeWord[type.value]} не более ${MaxPriceAmount.MAX_PRICE[type.value]}р`
+    : `Стоимость ${ChangeWord[type.value]} не меньше ${MinPriceAmount.MIN_PRICE[type.value]}р`;
+}
+
+pristine.addValidator(fieldPrice, validatePrice, errorMessagePrice);
+
+timeIn.addEventListener('change', () => {
+  timeOut.value = timeIn.value;
+  pristine.validate();
+});
+
+timeOut.addEventListener('change', () => {
+  timeIn.value = timeOut.value;
+  pristine.validate();
+});
+
+function validateTime () {
+  return  timeIn.value === timeOut.value;
+}
+
+function errorTime () {
+  return 'Время должно быть одинаково';
+}
+
+pristine.addValidator(timeOut, validateTime, errorTime);
 
 function addValidatorsToForm () {
   adForm.addEventListener('submit', () => {
